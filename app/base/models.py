594f419d016b9f931951794065db1e2691cb72de
lucas <lucas@Lucass-MacBook-Pro.local>
2018-08-05 08:49:32 +0800

    test:add map test

--- app/base/models.py
+++ app/base/models.py
@@ -6,7 +6,26 @@
 # @Desc  :
 
 
-class Restaurant:
+class BusyLevel:
     def __init__(self):
         pass
 
+    CLOSE = 0
+    OPEN = 0
+
+
+class Restaurant:
+    """ Restaurant """
+
+    id = 0
+    oid = 0
+    is_valid = 0
+    agent_fee = 0
+    busy_level = BusyLevel.CLOSE
+    description = ''
+    address = ''
+    image_url = ''
+    name = ''
+
+    def __init__(self):
+        pass
