2061856bc2d8a20ec8b87f1a71a5da1108d26b97
Lucas Ley <ayaaa168@163.com>
2018-08-09 23:10:30 +0800

    test:add random test with numpy

---
+++ app/base/randoms.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : randoms.py
+# @Author: lucas
+# @Date  : 8/9/18
+# @Desc  :
+
+
+import random
+import numpy as np
+import pprint
+
+
+def _numpy_random():
+    random_number = np.random.random_integers(0, 100)
+    pprint.pprint(random_number)
+
+
+def run():
+    values = list(range(0, 100))
+    random_value = random.choice(values)
+    pprint.pprint(random_value)
+    _numpy_random()
+
+
+if __name__ == '__main__':
+    run()
