b9d78aee41c5127f140eabf12524a6c81c5212ca
Lucas Ley <ayaaa168@163.com>
2018-08-28 18:14:41 +0800

    test:add map reduce test

--- app/base/func_map_reduce_super.py
+++ app/base/func_map_reduce_super.py
@@ -18,10 +18,17 @@ def char2num(s):
     return digits[s]
 
 
+def str2int(s):
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+
 def _mock():
     str_input = '14689'
     result = reduce(fn, map(char2num, str_input))
     pprint.pprint(result)
+
+    result = str2int('9876')
+    pprint.pprint('result={result},type={raw_type}'.format(result=result, raw_type=type(result)))
 
 
 def run():
