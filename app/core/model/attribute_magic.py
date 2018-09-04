1a5350ed077b4d388f2a9cd0ccf1aff92eddb32f
Lucas Ley <ayaaa168@163.com>
2018-09-04 11:45:01 +0800

    test:add robot class

--- app/core/model/attribute_magic.py
+++ app/core/model/attribute_magic.py
@@ -9,9 +9,12 @@ from instance_init import Student
 
 
 class Assistant(Student):
-    def __init__(self, name, score, title):
+    def __init__(self, id, name, score, title):
         super(Student, self).__init__()
+        self.id = id
         self.title = title
+        self.name = name
+        self.score = score
 
     def __str__(self):
         return str(self.__dict__)
@@ -20,10 +23,23 @@ class Assistant(Student):
         return str(self.__dict__)
 
 
+def access_attr():
+    import random
+    uid = random.randint(100, 1000)
+    assistant = Assistant(uid, 'elon', 80, 'Assistant')
+    for k in ['title', 'id', 'phone']:
+        if hasattr(assistant, k):
+            v = getattr(assistant, k)
+            print v
+
+
 def mock():
     anderson = Student('Bart Simpson', 90)
     print anderson
-    assistant = Assistant('elon', 80, 'Assistant')
+
+    import random
+    uid = random.randint(100, 1000)
+    assistant = Assistant(uid, 'elon', 80, 'Assistant')
     import pprint
     pprint.pprint(assistant)
     pprint.pprint(assistant.get_id())
@@ -31,6 +47,7 @@ def mock():
 
 def run():
     mock()
+    access_attr()
 
 
 if __name__ == '__main__':
---
+++ app/core/model/robot.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : robot.py
+# @Author: lucas
+# @Date  : 9/4/18
+# @Desc  :
+
+
+class Robot(object):
+    population = 0
+
+    def __init__(self, name):
+        self.name = name
+        import pprint
+        pprint.pprint('{name} Initializing.'.format(name=name))
+        Robot.population += 1
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def die(self):
+        """I'm dying!"""
+        print '{name} is being destroyed!'.format(name=self.name)
+        Robot.population -= 1
+        if Robot.population == 0:
+            print '{name} is the last one!'.format(name=self.name)
+        else:
+            print 'there are still {number} working!'.format(number=Robot.population)
+
+    def say_hi(self):
+        import pprint
+        pprint.pprint('hello,my master call me {name}'.format(name=self.name))
+
+    @classmethod
+    def how_many(cls):
+        print 'we have {population} robot(s)'.format(population=Robot.population)
+        return Robot.population
+
+
+def run():
+    megatron = Robot('Megatron')
+    blaster = Robot('Blaster')
+    print megatron
+    Robot.how_many()
+    megatron.die()
+    Robot.how_many()
+    blaster.die()
+
+
+if __name__ == '__main__':
+    run()
