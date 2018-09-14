b41f59c3465c8fda8c71cd1a6d5d8801dd42cc2f
Lucas Ley <ayaaa168@163.com>
2018-09-14 18:11:22 +0800

    test:define a new list with Set

--- app/base/list_base_set.py
+++ app/base/list_base_set.py
@@ -32,12 +32,39 @@ class ListBaseSet(Set):
     def __repr__(self):
         return str(self.__dict__)
 
+    def add(self, value):
+        self.elements.append(value)
+
+    def remove(self, value):
+        self.elements.remove(value)
+
+    def reverse(self):
+        return self.elements.reverse()
+
+    def get(self, index):
+        if index is None:
+            raise ValueError('index must not be null!')
+
+        if not isinstance(index, int):
+            raise ValueError('index must be integer!')
+
+        if index < 0 or index > len(self.elements) - 1:
+            raise ValueError('index out of bound!')
+
+        return self.elements[index]
+
 
 def run():
     s1 = ListBaseSet('windows')
     s2 = ListBaseSet('navigator')
     pprint(s1)
     pprint(s2)
+    s1.add(['X', 'Y', 'Z'])
+    pprint(s1)
+    s1.remove('w')
+    pprint(s1)
+    s1.get(1)
+    pprint(s1.get(3))
 
 
 if __name__ == '__main__':
